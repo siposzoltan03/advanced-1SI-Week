a3abc0db9326be8b492791ee3d0990585036915a
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-01-14 17:44:14 +0100

    capitalize member names

--- CreateClass/Person.cs
+++ CreateClass/Person.cs
@@ -4,26 +4,26 @@ namespace CreateClass
 {
     public class Person
     {
-        private string name;
-        private DateTime birthDate;
-        private Enum gender;
+        private string Name;
+        private DateTime BirthDate;
+        private Enum Gender;
 
         public Person(string name, DateTime birthDate, Enum gender)
         {
-            this.name = name;
-            this.birthDate = birthDate;
-            this.gender = gender;
+            this.Name = name;
+            this.BirthDate = birthDate;
+            this.Gender = gender;
         }
 
         public override string ToString()
         {
-            return $"Name: {this.name}\nBirthdate: {this.birthDate}\nGender: {this.gender}\n";
+            return $"Name: {this.Name}\nBirthdate: {this.BirthDate}\nGender: {this.Gender}\n";
         }
 
         public static void Main(string[] args)
         {
-            Person male = new Person("Mike", new DateTime(1990, 12, 14), Gender.Male);
-            Person female = new Person("Katy", new DateTime(1984, 04, 21), Gender.Female);
+            Person male = new Person("Mike", new DateTime(1990, 12, 14), CreateClass.Gender.Male);
+            Person female = new Person("Katy", new DateTime(1984, 04, 21), CreateClass.Gender.Female);
 
             Console.WriteLine(male.ToString());
             Console.WriteLine(female.ToString());
