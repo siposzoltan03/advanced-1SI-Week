62fb5cf73a10aa814c3f3965e96d6cfc987ec9bb
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-01-15 12:35:42 +0100

    implement exercise 4

---
+++ TimerEvents/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TimerEvents
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private DispatcherTimer dispatcherTimer;
+
+        private void MyProgressBar_Loaded(object sender, RoutedEventArgs e)
+        {
+            dispatcherTimer = new DispatcherTimer();
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1);
+            dispatcherTimer.Tick += new EventHandler(dispatcherTimer_Tick);
+            dispatcherTimer.Start();
+        }
+
+        private void dispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            myProgressBar.Value += 10;
+            if (myProgressBar.Value > 100)
+            {
+                dispatcherTimer.Stop();
+            }
+        }
+    }
+}
