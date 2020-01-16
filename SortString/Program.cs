c06ff865876157148f7d89da509d931aad94c177
siposzoltan03 <sipos.zoltan03@gmail.com>
2020-01-16 11:26:15 +0100

    Implement exercise 5

---
+++ SortString/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Microsoft .NET Framework 2.0 Application Development Foundation";
+            string[] words = str.Split(' ');
+
+            Array.Sort(words);
+
+            var s = string.Join(" ", words);
+            Console.WriteLine(s);
+
+        }
+    }
+}
